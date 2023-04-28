c3e456680c74e659751aff85dee727c882983b1c
Emmanuel Ozeh <ekemanuel.dev@gmail.com>
2023-04-28 12:21:47 +0100

    Set up hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,37 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
-};
+  defaultNetwork: "Mumbai",
+  networks: {
+    hardhat: {
+    },
+    Mumbai: {
+      url: process.env.MUMBAI_URL,
+      accounts : [ process.env.MNEMONIC ]
+    },
+    PolygonPoS: {
+      url: process.env.POLYGON_URL,
+      accounts : [ process.env.MNEMONIC ]
+    }
+
+    
+  },
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    },
+  },
+
+  etherscan: {
+    apiKey: process.env.API_KEY,
+  },
+  mocha: {
+    timeout: 40000
+  }
+}
