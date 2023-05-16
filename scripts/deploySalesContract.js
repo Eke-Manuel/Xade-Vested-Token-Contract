270c34bb9b9a1f576db78545bdcabfb8bb61a4b0
Emmanuel Ozeh <ekemanuel.dev@gmail.com>
2023-05-16 18:43:07 +0100

    Use OZ hardhat-upgrades

--- scripts/deploySalesContract.js
+++ scripts/deploySalesContract.js
@@ -1,13 +1,23 @@
-const hre = require("hardhat");
+const { ethers, upgrades } = require("hardhat");
+
+async function getImplementation(proxyAddr) {
+  const proxyAdmin = await upgrades.admin.getInstance();
+
+  return proxyAdmin.getProxyImplementation(proxyAddr)
+}
 
 async function main() {
 
-  const XadeTokenSales = await hre.ethers.getContractFactory("XadeTokenSales");
-  const xadeTokenSales = await XadeTokenSales.deploy("0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174","0x762d10549A7Ed1a502d6dF2A50C2fD70dAfe9744");
+  const xadeTokenSales = await ethers.getContractFactory("XadeTokenSales");
+  console.log("Deploying token sales...");
+  const XadeTokenSales = await upgrades.deployProxy(xadeTokenSales, ["0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174", "0x762d10549A7Ed1a502d6dF2A50C2fD70dAfe9744"], {initializer : "initialize" });
+  await XadeTokenSales.deployed();
 
-  await xadeTokenSales.deployed();
+  const impAddr = await getImplementation(XadeTokenSales.address);
 
-  console.log(xadeTokenSales.address);
+  console.log(`XadeTokenSales deployed to => Proxy: ${XadeTokenSales.address}, implementation: ${impAddr}`);
+
+  return XadeTokenSales.address
 }
 
 main().catch((error) => {
